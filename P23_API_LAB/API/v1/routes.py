6a6b7f9285cea7b8243595a454cc556e81576059
Henry Esteban Hormaza Salazar <henry@localhost.localdomain>
2020-07-26 13:07:52 -0400

    New POST method added to routes

--- P23_API_LAB/API/v1/routes.py
+++ P23_API_LAB/API/v1/routes.py
@@ -6,7 +6,7 @@ import urllib.parse
 #Using blueprint to soport future API versions and routes management at __init__
 people_bp = Blueprint('people_bp',__name__)
 
-@people_bp.route('/people/<int:nationalId>',methods=['GET'])
+@people_bp.route('/people/<string:nationalId>',methods=['GET','PUT'])
 def get_people_id(nationalId):
     #creating the object
     db_obj = sql_query.cls_sql()
@@ -16,10 +16,9 @@ def get_people_id(nationalId):
             db_obj.create_table() 
             try:         
                 #Retrive all the names
-                #nationalId = urllib.parse.unquote(request.args.get('nationalId'))
                 #query
-                db_obj.query_text = '''SELECT * FROM people_table 
-                                    where National_Id = '''+ str(nationalId)
+                db_obj.query_text = f'''SELECT * FROM {db_obj.table_name} 
+                                    where National_Id = '{nationalId}' '''
                 response["results"]= db_obj.execute_read_query()
                 if response["results"] != []:
                     return response,200
@@ -27,14 +26,14 @@ def get_people_id(nationalId):
                     return "Not found",404
             except:
                 #Retrive all the names
-                db_obj.query_text = '''SELECT * FROM people_table'''
+                db_obj.query_text = f'''SELECT * FROM {db_obj.table_name} '''
                 response["results"]=db_obj.execute_read_query()
                 return response,200
     except:
         return "Not id found",404
 
 #Provide info to api call
-@people_bp.route('/people',methods=['GET'])
+@people_bp.route('/people',methods=['GET', 'POST'])
 def get_people():
     #creating the object
     db_obj = sql_query.cls_sql()
@@ -43,7 +42,7 @@ def get_people():
         if request.method == 'GET': #check API method  
             db_obj.create_table() 
             #Retrive all the names
-            db_obj.query_text = '''SELECT * FROM people_table'''
+            db_obj.query_text = f'''SELECT * FROM {db_obj.table_name} '''
             response["results"]=db_obj.execute_read_query()
             return response,200
     except:
@@ -59,28 +58,34 @@ def get_people():
                     if request.headers["Content-Type"] == "application/json":
                         db_obj.query_text = db_obj.j_body
                     else:
-                        return "",500
+                        return "",400
                 except:
                     return "",500
 
                 nationalId = db_obj.j_body["nationalId"]
                 name = db_obj.j_body["name"]
+                lastName = db_obj.j_body["lastName"]
                 age = db_obj.j_body["age"]
                 pictureUrl= db_obj.j_body["pictureUrl"]
                 #Add new record
-                db_obj.query_text =  '''
+                db_obj.query_text =  f'''
                 INSERT INTO
-                `people_table` (`National_Id`, `Name`, `Last_Name`, `Age`, `Picture_Url`)
-                VALUES
-                ('''+nationalId+', '+name+', '+age+', '+pictureUrl+')'''
+                `{db_obj.table_name}` (`National_Id`, `Name`, `Last_Name`, `Age`, `Picture_Url`)
+                VALUES 
+                ('{nationalId}', '{name}', '{lastName}', {age}, '{pictureUrl}')'''
                 
-                db_obj.execute_query()  
-                response = {}
-                response["results"]= db_obj.execute_read_query() != []
-                if response["results"] != []:
+                db_obj.execute_query() 
+                db_obj.query_text = f'''SELECT * FROM {db_obj.table_name} 
+                                    where National_Id = '{nationalId}' '''
+                response["results"]= []
+                response["results"]= db_obj.execute_read_query()                                 
+                if response["results"] != [] and db_obj.error == "":
                     return response,201
                 else:
-                    return "Not id found",404
+                    if db_obj.error.errno != 1062:
+                        return f"Something went wrong",500
+                    else:
+                        return f"The error '{db_obj.error.msg}' occurred",500
             except:
                 #Retrive all the names
                 return response,400
